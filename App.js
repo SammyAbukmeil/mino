520da0482be04b6353d61de85c0682243ea1ff3e
Sammy Abukmeil <sammyabukmeil@gmail.com>
2022-11-23 15:19:37 +0000

    Add menu

--- App.js
+++ App.js
@@ -1,28 +1,72 @@
-import { extendTheme, NativeBaseProvider, ScrollView } from "native-base";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  AspectRatio,
+  Box,
+  Flex,
+  HamburgerIcon,
+  Image,
+  Menu,
+  NativeBaseProvider,
+  Pressable,
+  ScrollView,
+} from "native-base";
 import React from "react";
 import Block from "./components/Block/Block";
 
-const theme = extendTheme({});
+const logo = require("./assets/mino-logo.png");
+const foodImg = require("./assets/food.jpg");
+const sleepImg = require("./assets/sleep.jpg");
+const outsideImg = require("./assets/outside.jpg");
 
-// const foodImg = require("./assets/food.jpg");
-const foodImg =
-  "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=781&q=80";
-
-// const sleepImg = require("./assets/sleep.jpg");
-const sleepImg =
-  "https://images.unsplash.com/photo-1505693416388-ac5ce068fe85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
-
-const outsideImg =
-  "https://images.unsplash.com/photo-1594498653385-d5172c532c00?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80";
+const HomeScreen = () => {};
 
 export default () => {
   return (
-    <NativeBaseProvider>
-      <ScrollView>
-        <Block img={foodImg} textToSpeak="I want food" />
-        <Block img={sleepImg} textToSpeak="I want to sleep" />
-        <Block img={outsideImg} textToSpeak="I want to go outside" />
-      </ScrollView>
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <NativeBaseProvider>
+        <Flex
+          direction="row"
+          justify="space-between"
+          borderColor="coolGray.200"
+          borderWidth="1"
+          backgroundColor="#f9f9f9"
+        >
+          <Box w="15%" m="5">
+            <AspectRatio w="100%" ratio={3 / 3}>
+              <Image
+                source={{
+                  uri: logo,
+                }}
+                alt="image"
+              />
+            </AspectRatio>
+          </Box>
+          <Box w="25%" m="10" alignItems="flex-end">
+            <Menu
+              w="190"
+              trigger={(triggerProps) => {
+                return (
+                  <Pressable
+                    accessibilityLabel="More options menu"
+                    {...triggerProps}
+                  >
+                    <HamburgerIcon />
+                  </Pressable>
+                );
+              }}
+            >
+              <Menu.Item>Home</Menu.Item>
+              <Menu.Item>About</Menu.Item>
+              <Menu.Item>Contact</Menu.Item>
+            </Menu>
+          </Box>
+        </Flex>
+        <ScrollView>
+          <Block img={foodImg} textToSpeak="I want food" />
+          <Block img={sleepImg} textToSpeak="I want to sleep" />
+          <Block img={outsideImg} textToSpeak="I want to go outside" />
+        </ScrollView>
+      </NativeBaseProvider>
+    </NavigationContainer>
   );
 };
