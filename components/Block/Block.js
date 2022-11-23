d1eef51be31cd00d6a9b32c707a58aac3965059f
Sammy Abukmeil <sammyabukmeil@gmail.com>
2022-11-23 13:58:40 +0000

    Simplify block component

--- components/Block/Block.js
+++ components/Block/Block.js
@@ -1,44 +1,25 @@
 import * as Speech from "expo-speech";
-import { AspectRatio, Box, Heading, Image, Stack } from "native-base";
+import { AspectRatio, Box, Heading, Image } from "native-base";
 import React from "react";
-
 import { TouchableWithoutFeedback } from "react-native";
 
-const Block = ({ img, textToSpeak }) => {
-  const speak = () => {
-    Speech.speak(textToSpeak);
-  };
-
-  return (
-    <TouchableWithoutFeedback onPress={speak}>
-      <Box justifyContent="center" mt="20">
-        <Box
-          rounded="lg"
-          overflow="hidden"
-          borderColor="coolGray.200"
-          borderWidth="1"
-        >
-          <Box>
-            <AspectRatio w="100%" ratio={16 / 9}>
-              <Image
-                source={{
-                  uri: img,
-                }}
-                alt="image"
-              />
-            </AspectRatio>
-          </Box>
-          <Stack p="4" space={3}>
-            <Stack space={2}>
-              <Heading size="md" textAlign="center">
-                {textToSpeak}
-              </Heading>
-            </Stack>
-          </Stack>
-        </Box>
+const Block = ({ img, textToSpeak }) => (
+  <TouchableWithoutFeedback onPress={() => Speech.speak(textToSpeak)}>
+    <Box rounded="lg" borderColor="coolGray.200" borderWidth="1" mt="20">
+      <Box>
+        <AspectRatio w="100%" ratio={16 / 9}>
+          <Image
+            source={{
+              uri: img,
+            }}
+            alt="image"
+          />
+        </AspectRatio>
+        <Heading size="md" textAlign="center" p="4">
+          {textToSpeak}
+        </Heading>
       </Box>
-    </TouchableWithoutFeedback>
-  );
-};
-
+    </Box>
+  </TouchableWithoutFeedback>
+);
 export default Block;
