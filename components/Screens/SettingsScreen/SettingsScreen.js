663e6b50ee582507a9aa89e6662a9cb2ccfe235d
Sammy Abukmeil <sammyabukmeil@gmail.com>
2022-12-06 15:24:37 +0000

    Add image picker

--- components/Screens/SettingsScreen/SettingsScreen.js
+++ components/Screens/SettingsScreen/SettingsScreen.js
@@ -1,6 +1,7 @@
 import { Switch, Text } from "@rneui/themed";
+import * as ImagePicker from "expo-image-picker";
 import { useContext, useEffect, useState } from "react";
-import { ScrollView } from "react-native";
+import { Button, Image, ScrollView, View } from "react-native";
 import { ThemeContext } from "../../../contexts/ThemeContext";
 import useOverlay from "../../../hooks/useOverlay";
 import Header from "../../Header/Header";
@@ -9,6 +10,7 @@ import Menu from "../../Menu/Menu";
 const SettingsScreen = ({ navigation }) => {
   const [visible, toggleOverlay] = useOverlay();
   const [checked, setChecked] = useState(false);
+  const [image, setImage] = useState(null);
 
   const { toggleTheme, theme } = useContext(ThemeContext);
 
@@ -22,6 +24,22 @@ const SettingsScreen = ({ navigation }) => {
       setChecked(true);
     }
   }, []);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
 
   return (
     <>
@@ -41,15 +59,34 @@ const SettingsScreen = ({ navigation }) => {
           value={checked}
           onValueChange={toggleSwitch}
         />
-        {/* <Text
-          style={{
-            paddingHorizontal: 20,
-            paddingBottom: 20,
-            fontSize: 20,
-          }}
+        {/* 
+          Nicer Dark Theme:
+          https://react-native-async-storage.github.io/async-storage/docs/install
+        */}
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
         >
-          Two Blocks Per Row?
-        </Text> */}
+          <Button title="Pick an image from camera roll" onPress={pickImage} />
+          {image && (
+            <Image
+              source={{ uri: image }}
+              style={{ width: 200, height: 200 }}
+            />
+          )}
+        </View>
+        {/* 
+          TODO: BASIC STORAGE 
+          https://react-native-async-storage.github.io/async-storage/docs/usage
+        */}
+        {/* 
+          TODO: IMAGE STORAGE 
+          https://docs.expo.dev/versions/latest/sdk/filesystem/
+          https://stackoverflow.com/questions/62763209/how-do-i-save-an-image-from-expo-image-picker-to-expo-file-system-and-then-rende
+        */}
+        {/* 
+          TODO: Resize
+          https://docs.expo.dev/versions/v47.0.0/sdk/imagemanipulator/ 
+        */}
       </ScrollView>
       <Menu
         navigation={navigation}
